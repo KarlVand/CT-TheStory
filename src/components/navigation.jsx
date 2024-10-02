d6e0666af83b3a838823d629789fd2ad6d4d93b8
KarlVand <cg.vandooren.karl@gmail.com>
2024-10-02 21:46:04 +0200

    menu display / positioning

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,11 +1,7 @@
 import "../css/navigation.css";
-import Button from "./button.jsx";
 import Logo from "../assets/images/logoPlaceholder.webp";
 
 const Navigation = () => {
-  const handleClick = () => {
-    console.log("Button clicked!");
-  };
   return (
     <div className="navbar">
       <button className="toggle">
@@ -15,44 +11,39 @@ const Navigation = () => {
           className="logo"></img>
       </button>
 
-      <ul className="menu">
-        <li>
-          <Button
-            label="Home"
-            onClick={handleClick}
-          />
-        </li>
-        <li>
-          <Button
-            label="About"
-            onClick={handleClick}
-          />
-        </li>
-        <li>
-          <Button
-            label="Comic"
-            onClick={handleClick}
-          />
-        </li>
-        <li>
-          <Button
-            label="Runner"
-            onClick={handleClick}
-          />
-        </li>
-        <li>
-          <Button
-            label="Gallery"
-            onClick={handleClick}
-          />
-        </li>
-        <li>
-          <Button
-            label="Credits"
-            onClick={handleClick}
-          />
-        </li>
-      </ul>
+      <nav className="menu">
+        <a
+          href="#"
+          className="menuBtn">
+          Home
+        </a>
+        <a
+          href="#"
+          className="menuBtn">
+          About
+        </a>
+        <a
+          href="#"
+          className="menuBtn">
+          Comic
+        </a>
+        <a
+          href="#"
+          className="menuBtn">
+          Runner
+        </a>
+        <a
+          href="#"
+          className="menuBtn">
+          Gallery
+        </a>
+        <a
+          href="#"
+          className="menuBtn">
+          Credits
+        </a>
+        <span></span>
+      </nav>
     </div>
   );
 };
